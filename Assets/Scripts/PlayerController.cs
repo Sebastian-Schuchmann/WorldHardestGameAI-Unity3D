f6d6db1233ee9933ef6bde67d3b0c29444387c01
Sebastian Schuchmann <schuchmannsebastian@gmail.com>
2018-07-30 23:30:43 +0200

    Imported ML Agents and implemented basic KI

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MLAgents;
+
+public class PlayerController : MonoBehaviour
+{
+    private const int ENEMY_LAYER = 10;
+    private const int GOAL_LAYER = 8;
+
+    public WHDAgent agent; 
+    public float speed = 0.07f;
+    public float minStepToImprove = 0.02f;
+    public Text progressText;
+    public Text failCounterText;
+    private float lastProgress = 0.0f;
+    private int failCounter = 0;
+
+    public float HorizontalInput = 0.0f;
+    public float VerticalInput = 0.0f;
+
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float movementX = HorizontalInput * speed;
+        float movementY = VerticalInput * speed;
+
+        transform.position = transform.position + new Vector3(movementX, movementY, 0);
+        progressText.text = getProgress().ToString();
+        failCounterText.text = failCounter.ToString();
+
+        //Negative Reward for Time
+        agent.AddReward(-0.001f);
+
+        //Reward for moving towards the Goal
+        if(getProgress() > lastProgress+minStepToImprove){
+            agent.AddReward(0.02f);
+            lastProgress = getProgress();
+        } else if(getProgress() < lastProgress - minStepToImprove){
+            agent.AddReward(-0.02f);
+            lastProgress = getProgress();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == ENEMY_LAYER){
+            Kill();
+            agent.AddReward(-1.0f);
+            failCounter++;
+        }
+            
+        if (collision.gameObject.layer == GOAL_LAYER){
+            //WIN
+            failCounter = 0;
+            agent.AddReward(1.0f);
+            Kill();
+        }
+            
+    }
+
+    public void Kill(){
+        transform.position = startPosition;
+    }
+
+    public float getProgress(){
+        float progress = (transform.position.x - startPosition.x)/11.5f;
+        return Mathf.Clamp01(progress);
+    }
+}
+
+
+
---
+++ Assets/Scripts/WHDAgent.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class WHDAgent : Agent {
+
+    private PlayerController playerController;
+    public float viewRadius = 0.0f;
+
+    void Start(){
+        
+    }
+
+    public override void InitializeAgent()
+    {
+        playerController = GetComponent<PlayerController>();
+
+    }
+
+    public override void AgentReset()
+    {
+        playerController.Kill();
+    }
+
+    public override void CollectObservations()
+    {
+        Vector2[] rayDirections = {
+            Vector2.left,
+            Vector2.right,
+            Vector2.up,
+            Vector2.down
+        };
+
+        List<float> observations = new List<float>();
+        foreach(Vector2 direction in rayDirections)
+            observations.AddRange(CollectRayInformation(direction));
+
+        AddVectorObs(observations);
+
+    }
+
+    public List<float> CollectRayInformation(Vector2 direction){
+
+        List<float> observations = new List<float>();
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(currentPosition, viewRadius, direction);
+        foreach(RaycastHit2D hit in hits){
+            //Normalize Value between -1, 1
+            observations.Add((Vector2.Distance(transform.position, hit.point)-viewRadius/2)/8);
+            Debug.DrawLine(currentPosition, hit.point, Color.red);   
+        }
+
+        return observations;
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+       // playerController.HorizontalInput = vectorAction[0];
+       // playerController.VerticalInput = vectorAction[1];
+        switch((int)vectorAction[0]){
+            case 0:
+                playerController.VerticalInput = 1.0f;
+                playerController.HorizontalInput = 0f;
+                break;
+            case 1:
+                playerController.VerticalInput = -1.0f;
+                playerController.HorizontalInput = 0f;
+                break;
+            case 2:
+                playerController.HorizontalInput = 1.0f;
+                playerController.VerticalInput = 0.0f;
+                break;
+            case 3:
+                playerController.HorizontalInput = -1.0f;
+                playerController.VerticalInput = 0.0f;
+                break;
+            default:
+                playerController.VerticalInput = 0.0f;
+                playerController.HorizontalInput = 0f;
+                break;
+                
+        }
+    }
+}
